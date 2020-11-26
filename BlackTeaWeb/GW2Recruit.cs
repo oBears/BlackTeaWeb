3765adf5f30e59df0ff544e3197b6a4f7a181de4
许昶 <xv_chang@qq.com>
2020-11-26 12:26:41 +0800

    添加mongoDb支持

--- BlackTeaWeb/GW2Recruit.cs
+++ BlackTeaWeb/GW2Recruit.cs
@@ -2,54 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Driver;
 
 namespace BlackTeaWeb
 {
-	public class RecruitInfo
-	{
-		public int id;
+    public class RecruitInfo
+    {
+        public int id;
 
-		public long senderId;
+        public long senderId;
 
-		public string desc;
+        public string desc;
 
-		public override string ToString()
-		{
-			return $"【{id}】发布者:{senderId} [{desc}]";
-		}
-	}
+        public override string ToString()
+        {
+            return $"【{id}】发布者:{senderId} [{desc}]";
+        }
+    }
 
-	public static class GW2Recruit
-	{
-		private static List<RecruitInfo> recruitLst;
-		private static int generateId;
+    public static class GW2Recruit
+    {
+        private static List<RecruitInfo> recruitLst;
+        private static int generateId;
 
-		public static void Init()
-		{
-			recruitLst = new List<RecruitInfo>();
-		}
+        public static void Init()
+        {
+            recruitLst = new List<RecruitInfo>();
+        }
 
-		public static string GetRecruitLst()
-		{
-			string retStr = "目前列表:\r\n";
-			retStr += string.Join("\r\n", recruitLst);
+        public static string GetRecruitLst()
+        {
 
-			return retStr;
-		}
+            var db = MongoDbHelper.GetDb();
+            var recruits = db.GetCollection<RecruitInfo>("recruits").AsQueryable().ToList();
+            string retStr = "目前列表:\r\n";
+            retStr += string.Join("\r\n", recruits);
+            return retStr;
+        }
 
-		public static void InsertRecruit(long senderId, string rawMessage)
-		{
-			var splits = rawMessage.Split('|');
-			if (splits.Length > 1)
-			{
-				var desc = splits[1];
-				recruitLst.Add(new RecruitInfo() { senderId = senderId, desc = desc, id = generateId++ });
-			}
-		}
+        public static void InsertRecruit(long senderId, string rawMessage)
+        {
+            var splits = rawMessage.Split('|');
+            if (splits.Length > 1)
+            {
+                var desc = splits[1];
+                //recruitLst.Add(new RecruitInfo() { senderId = senderId, desc = desc, id = generateId++ });
 
-		public static RecruitInfo GetRecruitInfo(int id)
-		{
-			return recruitLst.Find((info)=> { return info.id == id; });
-		}
-	}
+                var db = MongoDbHelper.GetDb();
+                db.GetCollection<RecruitInfo>("recruits")
+                    .InsertOne(new RecruitInfo() { senderId = senderId, desc = desc, id = generateId++ });
+            }
+        }
+
+        public static RecruitInfo GetRecruitInfo(int id)
+        {
+
+            var db = MongoDbHelper.GetDb();
+           ;
+
+            //return recruitLst.Find((info) => { return info.id == id; });
+
+            return db.GetCollection<RecruitInfo>("recruits").Find(x => x.id == id).FirstOrDefault();
+        }
+    }
 }
---
+++ BlackTeaWeb/MongoDbHelper.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackTeaWeb
+{
+    public static class MongoDbHelper
+    {
+        private static string _connectionString;
+
+        public static void Init(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public static IMongoDatabase GetDb()
+        {
+            //与Mongodb建立连接
+            MongoClient client = new MongoClient(_connectionString);
+            //获得数据库,没有则自动创建
+            IMongoDatabase db = client.GetDatabase("blackTea");
+            return db;
+        }
+
+    }
+}
