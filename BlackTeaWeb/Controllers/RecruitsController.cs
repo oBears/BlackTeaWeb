a7429cce250a876af9167a9c678ddd6dd71d1475
许昶 <xv_chang@qq.com>
2020-12-09 17:21:31 +0800

    更换mvc

---
+++ BlackTeaWeb/Controllers/RecruitsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackTeaWeb.Controllers
+{
+    public class RecruitsController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
